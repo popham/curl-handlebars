2bc9ba9e0ae848ebdd54a89c766fa7e2160fd5ce
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2013-11-05 19:08:43 +0100

    Add test for ember handlebars plugin

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        mocha_phantomjs: {
+        	src: ['test/index.html']
+        }
+    });
+
+	grunt.loadNpmTasks('grunt-mocha-phantomjs');
+
+	grunt.registerTask('test', ['mocha_phantomjs']);
+};
---
+++ test/run.js
@@ -0,0 +1,53 @@
+curl({
+    "packages": {
+        "curl": {
+            "location": "../bower_components/curl/src/curl",
+        },
+        "ember": {
+            "location": "../bower_components/ember",
+            "main": "ember.min.js",
+            "config": {
+                "exports": "Ember",
+                "loader": "curl/loader/legacy",
+                "requires": ["jquery", "handlebars"]
+            }
+        },
+        "handlebars": {
+            "location": "../bower_components/handlebars",
+            "main": "handlebars.js",
+            "config": {
+                "dontWrapLegacy": true,
+                "exports": "Handlebars",
+                "loader": "curl/loader/legacy"
+            }
+        },
+        "chai": {
+            "location": "../bower_components/chai",
+            "main": "chai.js"
+        },
+        "mocha": {
+            "location": "../bower_components/mocha",
+            "main": "mocha.js",
+            "config": {
+                "exports": "mocha",
+                "loader": "curl/loader/legacy",
+                "requires": ["css!mocha/mocha"]
+            }
+        },
+        "ehbs": {
+            "location": "../src",
+            "main": "ehbs.js"
+        }
+    },
+    "paths": {
+        "jquery": "../bower_components/jquery/jquery.js"
+    },
+    "main": 'tests'
+}, ["mocha"], function(mocha) {
+    if (window.mochaPhantomJS) {
+        mochaPhantomJS.run();
+    }
+    else {
+        mocha.run();
+    }
+});
